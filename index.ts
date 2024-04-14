4b863a190b31d95c62fa8bd8a1ff11fae7743e3b
mrbilal457897 <163662455+mrbilal457897@users.noreply.github.com>
2024-04-14 13:27:01 +0500

    a todo list

---
+++ index.ts
@@ -0,0 +1,30 @@
+import inquirer from "inquirer"
+
+let todos = [];
+let condition = true;
+
+
+while(condition)
+{
+    let todoQuestion = await inquirer.prompt(
+    [
+        {
+        name: "firstQuestion",
+        type: "input",
+        message: "What you want to add in todo list?",
+        },
+
+        {
+        name: "secondQuestion",
+        type: "confirm",
+        message: "Do you want to add more",
+        default: "true",
+        }
+    ] 
+ );
+ 
+    todos.push(todoQuestion.firstQuestion);
+    condition = todoQuestion.secondQuestion;
+    console.log(todos);
+
+}
